e4d5071cf2a9a15f1daf2c66eea2f8091d3f80cc
Greg Ryan <gryan@vertafore.com>
2022-05-06 17:29:16 +0000

    Wait for Dropdown TC-539

--- src/test/java/serenitybase/steps/teststeps/ReportTemplateTestSteps.java
+++ src/test/java/serenitybase/steps/teststeps/ReportTemplateTestSteps.java
@@ -3,48 +3,14 @@ package serenitybase.steps.teststeps;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import java.util.UUID;
-import net.thucydides.core.annotations.Screenshots;
 import net.thucydides.core.annotations.Step;
-import org.apache.commons.lang3.StringUtils;
 import serenitybase.helpers.Utilities;
-import serenitybase.pages.mar.MarHomePage;
 import serenitybase.pages.mar.ReportTemplatePage;
 import serenitybase.pages.reports.CsvReport;
 import serenitybase.pages.reports.ExcelReport;
 
 public class ReportTemplateTestSteps {
-  private MarHomePage marHomePage;
   private ReportTemplatePage reportTemplatePage;
-  private String reportName;
-
-  @Step
-  public void selectReportTemplate(String reportTemplate) {
-    marHomePage.selectReportTemplate(reportTemplate);
-  }
-
-  @Step
-  public void generateReportWithRandomName() {
-    reportName = UUID.randomUUID().toString();
-    marHomePage.setReportName(reportName);
-    marHomePage.runReport();
-    marHomePage.waitForReportToComplete(reportName);
-  }
-
-  @Step
-  public void expandFilter(String filterName) {
-    marHomePage.expandFilter(filterName);
-  }
-
-  @Step
-  public void selectItemFromCategory(String itemName, String categoryName) {
-    marHomePage.selectItemFromCategory(itemName, categoryName);
-  }
-
-  @Step
-  public void selectBusinessUnitAssociationOption(String optionName) {
-    marHomePage.selectBusinessUnitAssociationOption(optionName);
-  }
 
   @Step
   public void selectTab(String tabName) {
@@ -52,32 +18,10 @@ public class ReportTemplateTestSteps {
   }
 
   @Step
-  public void excelQuickActions() {
-    marHomePage.clickExcelQuickActions(reportName);
-  }
-
-  @Step
-  public void csvQuickActions() {
-    marHomePage.clickCsvQuickActions(reportName);
-  }
-
-  @Step
-  @Screenshots(disabled = true)
-  public void navigateToGeneratedReport() {
-    marHomePage.clickOnReport(reportName);
-    Utilities.waitForReportPageLoadingSpinners();
-  }
-
-  @Step
   public void verifyNumberOfRows() {
     ExcelReport excelReport = new ExcelReport();
     assertThat(excelReport.getLastRowNum("Report Data"))
         .isEqualTo(reportTemplatePage.getNumberOfResults());
-  }
-
-  @Step
-  public void selectOptionUnderActions(String option) {
-    marHomePage.selectOptionUnderActions(option);
   }
 
   @Step
@@ -206,37 +150,6 @@ public class ReportTemplateTestSteps {
   @Step
   public void verifyColumnsAreDisplayedInTab(String columnName) {
     assertThat(reportTemplatePage.validateColumnsAreDisplayedInTab(columnName)).isTrue();
-  }
-
-  @Step
-  public void selectDateRangeFromList(String optionName) {
-    marHomePage.selectFromListOption();
-    marHomePage.clickFromListDropdown();
-    marHomePage.selectDateRangeOptionFromDropdown(optionName);
-  }
-
-  @Step
-  public void setDateRangeFromTo(String from, String to) {
-    marHomePage.selectSelectDatesOption();
-    marHomePage.setDateFrom(from);
-    marHomePage.setDateTo(to);
-  }
-
-  @Step
-  public void selectAccountNumbersSubLedgersFromDropdown(String option, String section) {
-    marHomePage.selectValueFromDropdown(option, section);
-  }
-
-  @Step
-  public void selectRandomAccountNumbersSubLedgersFromDropdown(String section) {
-    marHomePage.selectRandomValueFromDropdown(section);
-  }
-
-  @Step
-  public void verifyDropdownIsDisplayed(String section) {
-    if (StringUtils.isNotBlank(section)) {
-      assertThat(marHomePage.isDropdownDisplayed(section)).isTrue();
-    }
   }
 
   @Step
