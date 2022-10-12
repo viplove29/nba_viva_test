f01103cc2e97a901587c597b01a233dab48fd3b0
Greg Ryan <gryan@vertafore.com>
2022-10-12 14:06:53 +0000

    Work around for Login Issues

--- src/test/java/serenitybase/steps/teststeps/LoginToMarTestSteps.java
+++ src/test/java/serenitybase/steps/teststeps/LoginToMarTestSteps.java
@@ -1,6 +1,7 @@
 package serenitybase.steps.teststeps;
 
 import serenitybase.helpers.Configuration;
+import serenitybase.helpers.Utilities;
 import serenitybase.pages.mar.AgencySelectionPage;
 import serenitybase.pages.vsso.LoginPage;
 
@@ -15,5 +16,22 @@ public class LoginToMarTestSteps {
 
   public void selectAgency() {
     agencySelectionPage.selectAgency(Configuration.getAgency());
+  }
+
+  public void waitForAgencyPage() {
+    int counter = 0;
+    do {
+      String onURL = agencySelectionPage.getCurrentURL();
+      // Workaround for when VSSO login sends user to wrong page
+      if (onURL.contains("/Error")) {
+        System.out.println("Got Error Page - Selecting Back Button");
+        agencySelectionPage.backup();
+      } else if (onURL.contains("/SelectAgency")) {
+        return;
+      }
+      Utilities.simpleSleep(2000);
+      counter++;
+    } while (counter < 100);
+    throw new RuntimeException("Login Error - did not go to Select Agency page in 200 seconds");
   }
 }
