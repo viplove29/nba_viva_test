e2403018080546bcbba04795f9eef3ddb1c34dc3
Greg Ryan <gryan@vertafore.com>
2023-03-16 18:42:31 +0000

    Fix for wrong report type

--- src/test/java/serenitybase/steps/stepdefinitions/ExportOptionsStepDefinitions.java
+++ src/test/java/serenitybase/steps/stepdefinitions/ExportOptionsStepDefinitions.java
@@ -3,6 +3,7 @@ package serenitybase.steps.stepdefinitions;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
+import serenitybase.helpers.Utilities;
 import serenitybase.steps.teststeps.ExportOptionsTestSteps;
 
 public class ExportOptionsStepDefinitions {
@@ -10,6 +11,7 @@ public class ExportOptionsStepDefinitions {
 
   @When("use user exports the report with the following options")
   public void use_user_exports_the_report_with_the_following_options(DataTable options) {
+    Utilities.setDownloadsCount();
     exportOptionsTestSteps.selectExportOptions(options.asList());
   }
 }
